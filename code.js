c359decb38fbe6c08f84ce9ab8bc07aad405531c
AtharvGupta156 <89853442+AtharvGupta156@users.noreply.github.com>
2022-05-07 11:26:35 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+onEvent("button1", "click", function( ) {
+  open("https://youtu.be/WZ2Vn-DWFtY");
+});
+onEvent("button2", "click", function( ) {
+  open("https://youtu.be/gbNX1jPDzrY");
+});
+onEvent("button3", "click", function( ) {
+  open("https://youtu.be/jRccXHQD2Jc");
+});
+onEvent("button4", "click", function( ) {
+  open(" https://youtu.be/XUh-LxGlBG0");
+});
+onEvent("button5", "click", function( ) {
+  open("https://youtu.be/9sWz_orQ6uE");
+});
+onEvent("button1", "click", function( ) {
+  open("https://youtu.be/MmKE-uCtc7w");
+});
